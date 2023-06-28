6101f8f1b01fb7b2e8d49ed9e341244cf18b147b
Gaddam Rama Rao <ramarao.g92@gmail.com>
2023-06-28 15:34:12 +0530

    Update handler.js

--- handler.js
+++ handler.js
@@ -4,11 +4,11 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 //const routing = require("./routing/index");
-const bodyParser = require("body-parser");
+//const bodyParser = require("body-parser");
 // app.use(express.urlencoded({ extended: true }));
 // app.use(express.json());
-app.use(bodyParser.json());
-app.use(cors());
+//app.use(bodyParser.json());
+//app.use(cors());
 //app.use("/api", routing);
 app.get("/", (req, res)=>{
   res.json("Hello i am calling1111111111111");
